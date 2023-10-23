990d44ba1bf567ede3b527a4024ba198984bc469
Austin Abell <austinabell8@gmail.com>
2023-10-22 23:26:27 -0400

    Basic framework of poc

--- src/client.rs
+++ src/client.rs
@@ -1,2 +1,45 @@
-fn main() {
+use axum::body::Bytes;
+use rand::{rngs::StdRng, Rng, SeedableRng};
+use sha2::{Digest, Sha256};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // The list of node URLs
+    let nodes = vec!["http://localhost:3001", "http://localhost:3002"];
+
+    // Initialize the current randomness as all zeros
+    let mut current_randomness: Bytes = Bytes::from(&[0u8; 32][..]);
+
+    loop {
+        // Select a node based on some function of the current randomness
+        // For simplicity, let's just use the first byte to index into the nodes array
+        let array: [u8; 32] = Sha256::digest(&current_randomness).into();
+
+        let mut rng = StdRng::from_seed(array);
+        let random_value: usize = rng.gen();
+        let node_idx = random_value % nodes.len();
+        let node_url = nodes[node_idx];
+
+        // Perform HTTP request to the node
+        let client = reqwest::Client::new();
+        let res = client
+            .post(node_url)
+            .body(current_randomness)
+            .send()
+            // TODO gracefully handle errors
+            .await?;
+
+        // Assume the node returns a new random hash as a hex string
+        let new_random_hash: Bytes = res.bytes().await?;
+
+        // Update the current randomness
+        current_randomness = new_random_hash;
+
+        // Log the randomness and selected node
+        println!("Current randomness: {:?}", hex::encode(&current_randomness));
+        println!("Selected node: {}", node_url);
+
+        // Simulating continuous operation
+        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+    }
 }
--- src/node.rs
+++ src/node.rs
@@ -1,8 +1,49 @@
-fn main() {
-    // TODO Generate in-memory BLS key
-    
-    // TODO Set up HTTP server
-    
-    // TODO Define handler to accept incoming request with random bytes
-    // Generate random hash using BLS and return it
+use axum::body::Bytes;
+use axum::extract::State;
+use axum::{routing::post, Router};
+// TODO benchmark this against min_sig
+use blst::min_pk::{SecretKey, Signature};
+use rand::RngCore;
+use std::env;
+use std::sync::Arc;
+
+// TODO look into this and if it's necessary
+const DST: &[u8] = b"BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_";
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // TODO setup logging
+
+    let port = env::var("NODE_PORT").unwrap_or_else(|_| "3000".to_string());
+
+    // Generate in-memory BLS key
+    let mut rng = rand::thread_rng();
+    let mut ikm = [0u8; 32];
+    rng.fill_bytes(&mut ikm);
+
+    let sk = SecretKey::key_gen(&ikm, &[]).unwrap();
+    let state = Arc::new(sk);
+
+    // Build our application with a route
+    let app = Router::new()
+        .route("/", post(handle_randomness))
+        .with_state(state.clone());
+
+    // Run it with hyper
+    let addr = format!("0.0.0.0:{}", port).parse().unwrap();
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await?;
+    Ok(())
+}
+
+async fn handle_randomness(State(sk): State<Arc<SecretKey>>, body: Bytes) -> Bytes {
+    // Arbitrarily sign the message sent in
+    // NOTE: This is a terrible idea to do in any practical use case, but just to assume node is
+    // following and validating what is being signed.
+    let sig: Signature = sk.sign(&body, DST, &[]);
+
+    println!("{sig:?}");
+    // Return the encoded result
+    Bytes::copy_from_slice(&sig.to_bytes())
 }
